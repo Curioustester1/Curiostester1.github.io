6e4cb818d5e4cd8799e67d3c1458a3591e7ed739
Dhoo_ <103371177+Curioustester1@users.noreply.github.com>
2022-06-04 16:39:10 +0800

    Update Kalkulator.js

--- Kalkulator.js
+++ Kalkulator.js
@@ -1,1 +1,35 @@
-//testing doang dekk nih materi agak pusing juga mahamnya meskipun EZ 🗿//
+//testing doang dekk nih materi agak pusing juga mahamnya meskipun EZ 🗿
+
+const calculator = {
+   displayNumber: '0',
+   operator: null,
+   firstNumber: null,
+   waitingForSecondNumber: false
+};
+
+function updateDisplay() {
+   document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+ 
+function clearCalculator() {
+   calculator.displayNumber = '0';
+   calculator.operator = null;
+   calculator.firstNumber = null;
+   calculator.waitingForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+   calculator.displayNumber += digit;
+}
+
+const buttons = document.querySelectorAll(".button");
+for (let button of buttons) {
+   button.addEventListener('click', function(event) {
+ 
+       // mendapatkan objek elemen yang diklik
+       const target = event.target;
+ 
+       inputDigit(target.innerText);
+       updateDisplay()
+   });
+}
